443bae3282b88387d50215ff9b6bf3cd153ebe2a
Lite <lightwithoutlimit@proton.me>
2026-03-22 00:31:58 -0400

    Sync @phantasy/plugin-kalshi (source-extracted) from monorepo export

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,3 @@
-import { BasePlugin, PluginManifest, PluginTool } from "@phantasy/core";
-
-export class UkalshiPlugin extends BasePlugin {
-  readonly name = "kalshi";
-  readonly version = "1.0.0";
-
-  getManifest(): PluginManifest {
-    return {
-      name: this.name,
-      version: this.version,
-      description: "kalshi plugin for Phantasy",
-      author: "Phantasy",
-      license: "BUSL-1.1",
-      repository: "https://github.com/phantasy-bot/plugin-kalshi",
-    };
-  }
-
-  getTools(): PluginTool[] {
-    return [];
-  }
-
-  async initialize(): Promise<void> {
-    console.log("[UkalshiPlugin] Initialized");
-  }
-}
-
-export default UkalshiPlugin;
+export { KalshiPlugin } from "./kalshi-plugin";
+export { KalshiService } from "./kalshi-service";
+export { default } from "./kalshi-plugin";
